3f209d4a4103dd0267bab9afcfb12ab301d278bf
pouniq <abdollahpora@gmail.com>
2026-09-14 15:23:51 +0330

    added sql code

---
+++ dataAnalytics.sql
@@ -0,0 +1,7 @@
+USE fourth_grade;
+SELECT * FROM center; SELECT * FROM holy; SELECT * FROM math;
+SELECT * FROM persian; SELECT * FROM social; SELECT * FROM writing;
+
+SELECT * FROM sci;
+DELETE FROM sci WHERE id BETWEEN 37 AND 43;
+
